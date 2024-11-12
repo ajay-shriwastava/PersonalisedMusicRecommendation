388166863ad71e3c431f3c602670912193b23d45
Ajay Shriwastava <ashriwastava@gmail.com>
2024-11-12 22:34:22 +0530

    Added Preference Management in the UI

--- src/webUI/app.py
+++ src/webUI/app.py
@@ -1,65 +1,80 @@
-import db_cmds as db
 from flask import Flask, render_template, request, url_for, flash, redirect
 import json
 from werkzeug.exceptions import abort
-
+import db_cmds as db
+import lookup
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CDSCOHORT7GROUP7'
+
 DEFAULT_SESSION_USER_ID = "35653693"
 session_user_id = DEFAULT_SESSION_USER_ID
 
 @app.route('/', methods=('GET', 'POST'))
-def index():
+@app.route('/<int:session_user_id>', methods=('GET', 'POST'))
+def index(session_user_id=DEFAULT_SESSION_USER_ID):
     if request.method == 'POST':
         session_user_id = request.form['session_user_id']
-        session_user = db.get_user(session_user_id)
-    else:
+    if not session_user_id:
         session_user_id = DEFAULT_SESSION_USER_ID
-        session_user = db.get_user(session_user_id)
+    session_user = db.get_user(session_user_id)
+    prefStr = session_user['preferences']
+    prefDict = {}
+    if prefStr:
+        prefDict = json.loads(prefStr)
     users = db.get_all_users()
     top_tracks = db.get_top_tracks()
-    return render_template('index.html', session_user=session_user, top_tracks=top_tracks, users=users)
+    return render_template('index.html', session_user=session_user, preferences=prefDict, top_tracks=top_tracks,
+                           users=users)
+
 
 @app.route('/createUser', methods=('GET', 'POST'))
 def createUser():
     if request.method == 'POST':
         user_id = request.form['user_id']
-        location = request.form['location']
-        if not location:
-            flash('Location selection is required!')
+        locations = request.form.getlist('user_locations')
+        languages = request.form.getlist('user_languages')
+        genres = request.form.getlist('user_genres')
+        if not locations:
+            flash('Location is required!')
+        elif not languages:
+            flash('Language is required!')
         else:
-            db.insertUser(user_id, location)
-            return redirect(url_for('index'))
-    return render_template('createUser.html')
+            prefDict = {'locations': locations, 'languages': languages}
+            if genres:
+                prefDict['genres'] = genres
+            new_pref_str = json.dumps(prefDict)
+            db.insertUser(user_id, new_pref_str)
+            return redirect(url_for('index', session_user_id=user_id))
+    return render_template('createUser.html', locations_lookup=lookup.locations_lookup,
+                           languages_lookup=lookup.languages_lookup, genres_lookup=lookup.genres_lookup)
 
 
 @app.route('/user/<int:user_id>/edit', methods=('GET', 'POST'))
 def editUser(user_id):
     user = db.get_user(user_id)
+    prefStr = user['preferences']
+    prefDict = {}
+    if prefStr:
+        prefDict = json.loads(prefStr)
     if request.method == 'POST':
         tracks = request.form.getlist('user_tracks')
-        print("Type of tracks is", type(tracks), tracks)
         if not tracks:
             flash('Track selection is required!')
         else:
-            prefStr = user['preferences']
-            print("Existing Preference Dictionary", prefStr)
-            prefDict = {}
-            if prefStr:
-                prefDict = json.loads(prefStr)
             prefDict['tracks'] = tracks
-            print("Tracks updated", prefDict)
             new_pref_str = json.dumps(prefDict)
             db.editUser(user_id, new_pref_str)
-            return redirect(url_for('index'))
-    return render_template('editUser.html', user=user)
+            return redirect(url_for('index', session_user_id=user_id))
+    return render_template('editUser.html', user=user, preferences=prefDict, tracks_lookup=lookup.tracks_lookup)
+
 
 @app.route('/user/<int:user_id>/delete', methods=('POST',))
 def deleteUser(user_id):
     db.deleteUser(user_id)
     flash('"{}" was successfully deleted!'.format(user_id))
     return redirect(url_for('index'))
+
 
 @app.route('/about')
 def about():
--- src/webUI/sqlite/init_db.py
+++ src/webUI/sqlite/init_db.py
@@ -12,14 +12,6 @@
 #            )
 
 
-(174626103, "Great Portland Street", "City of Westminster", "England", "GB", "United Kingdom", "en-GB,cy-GB,gd"),
-(161262801, "Emil-Jannings-Straße", "Potsdam", "Brandenburg", "DE", "Germany", "de"),
-(174228242, "Great Portland Street", "City of Westminster", "England", "GB", "United Kingdom", "en-GB,cy-GB,gd"),
-(160874621, "Wetzlarer Straße", "Potsdam", "Brandenburg", "DE", "Germany", "de"),
-(35653693, "403 King Springs Village Pky SE", "Smyrna", "GA", "US", "United States", "en-US,es-US,haw,fr"),
-(259861614, "528 N Felton St", "Philadelphia", "PA", "US", "United States", "en-US,es-US,haw,fr"),
-(346364194, "599 Wythe Ave", "Brooklyn", "NY", "US", "United States", "en-US,es-US,haw,fr"),
-(181840782, "2153 County Line Rd", "Mt Gilead", "NC", "US", "United States", "en-US,es-US,haw,fr")
 #connection.commit()
 #connection.close()
 
